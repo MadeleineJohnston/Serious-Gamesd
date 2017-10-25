37690a9f1dfe6ef75f2747dc21912fba40872c80
garyanderson19 <b00266621@studentmail.uws.ac.uk>
2017-10-25 19:27:09 +0100

    Initial commit

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+	public Canvas instructionMenu;
+	public Button playText;
+	public Button instructionText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+
+		instructionMenu = instructionMenu.GetComponent<Canvas> ();
+		playText = playText.GetComponent<Button> ();
+		instructionText = instructionText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		instructionMenu.enabled = false;
+	}
+	
+	public void InstructionPress() {
+
+		instructionMenu.enabled = true;
+		playText.enabled = false;
+		instructionText.enabled = false;
+		exitText.enabled = false;
+
+	}
+
+	public void menuPress(){
+		
+			instructionMenu.enabled = false;
+			playText.enabled = true;
+			instructionText.enabled = true;
+			exitText.enabled = true;
+
+		}
+
+	public void StartLevel(){
+	
+		SceneManager.LoadScene ("Mini game");
+
+	}
+
+
+	public void ExitGame(){
+
+		Application.Quit ();
+
+	}
+}
